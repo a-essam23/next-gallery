a6f6a0c78129722bda796283383bb5037aef2f22
Ahmed Essam <bloxed7@gmail.com>
2022-10-22 12:22:08 +0200

    readme edits

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ const limiter = rateLimit({
     windowMs: 60 * 60 * 1000,
     message: "Too many requests from this IP,please try again in an hour!",
 });
-// app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: "10kb" }));
 app.use("/api", limiter);
 app.use(express.static(path.join(__dirname, `public`)));
 app.use(express.static(path.join(__dirname, `files`)));
