75e93f2e6ca48764f7ae05948451b15fd69c4e27
AbdurrahmanASUR <abdoasur@gmail.com>
2022-10-25 23:45:06 +0300

    init

--- components/models/ModelInfo.js
+++ components/models/ModelInfo.js
@@ -1,64 +1,50 @@
-import {
-    faFacebookMessenger,
-    faWhatsapp,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export default function ModelInfo({
-    data: { image, name, size = null },
-    className,
-}) {
-    ///// TODO MAKE NEW COMPONENT FOR MOB A
-    const [width, height, depth] = size ? size.split(",") : [];
-    // console.log(width, height, depth);
-    return (
-        <div
-            className={`py-0 sm:py-6 md:py-8 lg:flex lg:py-10 xl:px-8 xl:py-8 2xl:px-8 w-full 2xl:w-11/12 h-full overflow-scroll no-scrollbar lg:overflow-hidden ${+className}`}
-        >
-            <div className="basis-7/12 flex w-full justify-center items-center lg:shadow-md rounded lg:max-h-120 xl:max-h-144 2xl:max-h-full">
-                <img
-                    src={image}
-                    alt={name}
-                    className="object:cover w-full max-h-full md:rounded md:rounded-b-none lg:rounded-tr-none lg:rounded-l "
-                />
-            </div>
-            <div className="gap-2 md:gap-4 sm:rounded-b lg:rounded-bl-none lg:rounded-r flex flex-col basis-5/12 flex-grow lg:shadow-lgb">
-                <div className="grid grid-cols-3 gap-3 pt-4 sm:gap-6 2xl:gap-8 rounded">
-                    <div className="flex px-4 gap-5 sm:gap-8 2xl:gap-4 ">
-                        <a href="/facebook">
-                            <FontAwesomeIcon
-                                icon={faFacebookMessenger}
-                                color="#006AFF"
-                                className="w-6 sm:w-8 lg:w-6 h-auto"
-                            />
-                        </a>
-                        <a href="/whatsapp">
-                            <FontAwesomeIcon
-                                icon={faWhatsapp}
-                                color="green"
-                                className="w-6 sm:w-8 lg:w-6 h-auto"
-                            />
-                        </a>
-                    </div>
+import { faFacebookMessenger, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+export default function ModelInfo({ data: { image, name, size = null }, className }) {
+  ///// TODO MAKE NEW COMPONENT FOR MOB A
+  const [width, height, depth] = size ? size.split(',') : []
+  // console.log(width, height, depth);
+  return (
+    <div
+      className={`py-0 sm:py-6 md:py-8 lg:flex lg:py-10 xl:px-8 xl:py-8 2xl:px-8 w-full 2xl:w-11/12 h-full overflow-scroll no-scrollbar lg:overflow-hidden ${+className}`}
+    >
+      <div className="basis-7/12 flex w-full justify-center items-center lg:shadow-md rounded lg:max-h-120 xl:max-h-144 2xl:max-h-full">
+        <img
+          src={image}
+          alt={name}
+          className="object:cover w-full max-h-full md:rounded md:rounded-b-none lg:rounded-tr-none lg:rounded-l "
+        />
+      </div>
+      <div className="gap-2 md:gap-4 sm:rounded-b lg:rounded-bl-none lg:rounded-r flex flex-col basis-5/12 flex-grow lg:shadow-lgb">
+        <div className="grid grid-cols-3 gap-3 pt-4 sm:gap-6 2xl:gap-8 rounded">
+          <div className="flex px-4 gap-5 sm:gap-8 2xl:gap-4 ">
+            <a href="/facebook">
+              <FontAwesomeIcon icon={faFacebookMessenger} color="#006AFF" className="w-6 sm:w-8 lg:w-6 h-auto" />
+            </a>
+            <a href="/whatsapp">
+              <FontAwesomeIcon icon={faWhatsapp} color="green" className="w-6 sm:w-8 lg:w-6 h-auto" />
+            </a>
+          </div>
 
-                    <div className="items-center text-2xl sm:text-2xl md:text-3xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold text-center">
-                        {name}
-                    </div>
-                </div>
-
-                <div className="gap-2 sm:gap-4 items-center flex text-xl sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl font-bold justify-center">
-                    {width || ""}
-                    {height && "  x  "}
-                    {height || ""}
-                    {depth && "  x  "}
-                    {depth || ""}
-                </div>
-                <div className="overflow-y-scroll no-scrollbar bg-white border-t-2 w-full grow h-52 lg:h-full rounded relative ">
-                    <div className="top-4 left-4 absolute">Comments:</div>
-                    <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 -rotate-45 text-4xl w-full text-center">
-                        COMING SOON
-                    </div>
-                </div>
-            </div>
+          <div className="items-center text-2xl sm:text-2xl md:text-3xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold text-center">
+            {name}
+          </div>
         </div>
-    );
+
+        <div className="gap-2 sm:gap-4 items-center flex text-xl sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl font-bold justify-center">
+          {width || ''}
+          {height && '  x  '}
+          {height || ''}
+          {depth && '  x  '}
+          {depth || ''}
+        </div>
+        <div className="overflow-y-scroll no-scrollbar bg-white border-t-2 w-full grow h-52 lg:h-full rounded relative ">
+          <div className="top-4 left-4 absolute">Comments:</div>
+          <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 -rotate-45 text-4xl w-full text-center">
+            COMING SOON
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -1,112 +1,104 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { Navigation, Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { v4 } from "uuid";
-import {
-    EmptyPlaceHolder,
-    Layout,
-    Model,
-    ModelInfo,
-    Searchbar,
-} from "../../components";
-import { useLang } from "../../hooks";
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Navigation, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { v4 } from 'uuid'
+import { EmptyPlaceHolder, Layout, Model, ModelInfo, Searchbar } from '../../components'
+import { useLang } from '../../hooks'
 
 export async function getServerSideProps(context) {
-    const models = Array(10)
-        .fill()
-        .map((el, i) => ({
-            name: `Model ${i + 1}`,
-            image: "/imgs/placeholder1.jpg",
-            _id: i,
-            size: "120,150,40",
-        }));
-    return {
-        props: { models }, // will be passed to the page component as props
-    };
+  const models = Array(10)
+    .fill()
+    .map((el, i) => ({
+      name: `Model ${i + 1}`,
+      image: '/imgs/placeholder1.jpg',
+      _id: i,
+      size: '120,150,40',
+    }))
+  return {
+    props: { models }, // will be passed to the page component as props
+  }
 }
 
 export default function ModelPage({ models = [] }) {
-    const router = useRouter();
-    const collectionId = router.query.collectionId;
-    // const [searchParams, setSearchParams] = useSearchParams();
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [models, setmodels] = useState([]);
-    const [isShown, setIsShown] = useState(false);
-    const [slideIndex, setSlideIndex] = useState(1);
-    const { isAr } = useLang();
+  const router = useRouter()
+  const collectionId = router.query.collectionId
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [models, setmodels] = useState([]);
+  const [isShown, setIsShown] = useState(false)
+  const [slideIndex, setSlideIndex] = useState(1)
+  const { isAr } = useLang()
 
-    useEffect(() => {
-        if (router.query.ref) {
-            setSlideIndex(router.query.ref || 0);
-            setIsShown(true);
-        }
-        // eslint-disable-next-line
-    }, []);
-    useEffect(() => {
-        isShown
-            ? (document.body.style.overflowY = "hidden")
-            : (document.body.style.overflowY = "scroll");
-    }, [isShown]);
-    return (
-        <Layout>
-            <Searchbar
-                choices={["model"]}
-                onFinish={(data) => {
-                    setSearchParams({ [data.type]: data.value });
-                }}
+  useEffect(() => {
+    if (router.query.ref) {
+      setSlideIndex(router.query.ref || 0)
+      setIsShown(true)
+    }
+    // eslint-disable-next-line
+  }, [])
+  useEffect(() => {
+    isShown ? (document.body.style.overflowY = 'hidden') : (document.body.style.overflowY = 'scroll')
+  }, [isShown])
+  return (
+    <Layout>
+      <Searchbar
+        choices={['model']}
+        onFinish={data => {
+          setSearchParams({ [data.type]: data.value })
+        }}
+      />
+      {/* {isLoading && <Spin size="large" />} */}
+      {isShown && (
+        <>
+          <div
+            className="fixed bg-black opacity-50 w-screen h-screen left-0 top-0 cursor-pointer z-10 overflow-hidden"
+            onClick={() => {
+              setIsShown(false)
+            }}
+          ></div>
+          <div className="h-4/5 lg:h-auto xl:max-h-160 2xl:max-h-232 w-11/12 sm:w-9/12 md:w-8/12 lg:w-10/12 xl:w-9/12 2xl:w-8/12 lg:justify-center lg:items-center flex bg-white rounded m-auto z-20 fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 overflow-y-scroll no-scrollbar lg:overflow-y-hidden">
+            <Swiper
+              onSwiper={swiper => {
+                swiper.slideTo(slideIndex, 0)
+              }}
+              navigation
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination, Navigation]}
+              className="flex justify-cemter items-center"
+            >
+              {models.map(model => (
+                <SwiperSlide
+                  key={model._id}
+                  className="py-5 px-5 sm:px-10 md:px-12 lg:px-14 xl:py-0 xl:px-4 flex justify-center items-center max-h-full max-w-full"
+                >
+                  <ModelInfo data={model} className="" />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </>
+      )}
+      {models.length > 0 ? (
+        <section className="xl:pt-8 grid auto-rows-fr grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 z-0">
+          {models.map((model, i) => (
+            <Model
+              key={v4()}
+              data={model}
+              onClick={e => {
+                e.preventDefault()
+                console.log('clicked!!')
+                setIsShown(true)
+                setSlideIndex(i)
+              }}
             />
-            {/* {isLoading && <Spin size="large" />} */}
-            {isShown && (
-                <>
-                    <div
-                        className="fixed bg-black opacity-50 w-screen h-screen left-0 top-0 cursor-pointer z-10 overflow-hidden"
-                        onClick={() => {
-                            setIsShown(false);
-                        }}
-                    ></div>
-                    <div className="h-4/5 lg:h-auto xl:max-h-160 2xl:max-h-232 w-11/12 sm:w-9/12 md:w-8/12 lg:w-10/12 xl:w-9/12 2xl:w-8/12 lg:justify-center lg:items-center flex bg-white rounded m-auto z-20 fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 overflow-y-scroll no-scrollbar lg:overflow-y-hidden">
-                        <Swiper
-                            onSwiper={(swiper) => {
-                                swiper.slideTo(slideIndex, 0);
-                            }}
-                            navigation
-                            pagination={{
-                                clickable: true,
-                            }}
-                            modules={[Pagination, Navigation]}
-                            className="flex justify-cemter items-center"
-                        >
-                            {models.map((model) => (
-                                <SwiperSlide
-                                    key={model._id}
-                                    className="py-5 px-5 sm:px-10 md:px-12 lg:px-14 xl:py-0 xl:px-4 flex justify-center items-center max-h-full max-w-full"
-                                >
-                                    <ModelInfo data={model} className="" />
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                    </div>
-                </>
-            )}
-            {models.length > 0 ? (
-                <section className="xl:pt-8 grid auto-rows-fr grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 z-0">
-                    {models.map((model, i) => (
-                        <Model
-                            key={v4()}
-                            data={model}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                console.log("clicked!!");
-                                setIsShown(true);
-                                setSlideIndex(i);
-                            }}
-                        />
-                    ))}
-                </section>
-            ) : (
-                <EmptyPlaceHolder />
-            )}
-        </Layout>
-    );
+          ))}
+        </section>
+      ) : (
+        <EmptyPlaceHolder />
+      )}
+    </Layout>
+  )
 }
