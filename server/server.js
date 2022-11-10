c6d28f9966f77133b5f2e9b4e05a8447c694ead1
Ahmed Essam <bloxed7@gmail.com>
2022-11-10 19:29:19 +0200

    added server only enviroment

--- server/server.js
+++ server/server.js
@@ -1,57 +1,42 @@
-const next = require("next");
 const mongoose = require("mongoose");
-// require("dotenv").config();
+require("dotenv").config({ path: ".env.local" });
 
-const dev = process.env.NODE_ENV !== "production";
 let port = process.env.PORT || 3000;
 let hostname = process.env.HOST || "localhost";
-const nextServer = next({ dev, port, hostname });
-const handle = nextServer.getRequestHandler();
 
-nextServer
-    .prepare()
-    .then(async () => {
-        const app = require("./app");
-        const DB = process.env.DATABASE.replace(
-            "<password>",
-            process.env.DATABASE_PASSWORD
-            // console.log("Online database reported")
-        );
+const app = require("./app");
+const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+    // console.log("Online database reported")
+);
 
-        await mongoose
-            // .connect(process.env.DATABASE_LOCAL)
-            .connect(DB, { dbName: "roman" })
-            .then(() => {
-                console.log("DB connection successful!".cyan);
-            });
-
-        app.get("*", (req, res) => {
-            return handle(req, res);
-        });
-
-        app.listen(port, hostname, function () {
-            console.log(`Server running on ${hostname + ":" + port}`.green);
-        });
-
-        process.on("unhandledRejection", (err) => {
-            console.log("UNHANDLED REJECTION!");
-            console.log(err.stack);
-            server.close(() => {
-                process.exit(1);
-            });
-        });
-
-        process.on("uncaughtException", (err) => {
-            console.log("Uncaught expection!");
-            console.log(err.stack);
-            server.close(() => {
-                process.exit(1);
-            });
-        });
-    })
-    .catch((e) => {
-        console.log(e);
-        // process.exit(1);
+mongoose
+    // .connect(process.env.DATABASE_LOCAL)
+    .connect(DB, { dbName: "roman" })
+    .then(() => {
+        console.log("DB connection successful!".cyan);
     });
 
+app.listen(port, hostname, function () {
+    console.log(`Server running on ${hostname + ":" + port}`.green);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION!");
+    console.log(err.stack);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught expection!");
+    console.log(err.stack);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+module.exports = app;
 //TEST
---
+++ server/server_next.js
@@ -0,0 +1,18 @@
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const nextServer = next({ dev, port, hostname });
+const handle = nextServer.getRequestHandler();
+
+nextServer
+    .prepare()
+    .then(async () => {
+        const app = require("./server");
+        app.get("*", (req, res) => {
+            return handle(req, res);
+        });
+    })
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
