aa98168606c3dda7a191f5023261439ecc4f1808
Ahmed Essam <bloxed7@gmail.com>
2022-11-17 05:52:16 +0200

    minor fixes... 7

--- server/routers/admin/adminValidation.js
+++ server/routers/admin/adminValidation.js
@@ -1,14 +1,14 @@
 const Joi = require("joi");
 
 exports.updateUserValidation = {
-  body: Joi.object()
-    .required()
-    .keys({
-      name: Joi.string(),
-      email: Joi.string().email(),
-      facebookId: Joi.string(),
-      googleId: Joi.string(),
-      role: Joi.string().valid("user", "data-entry", "admin"),
-      active: Joi.boolean(),
-    }),
+    body: Joi.object()
+        .required()
+        .keys({
+            name: Joi.string(),
+            email: Joi.string().email(),
+            facebookId: Joi.string(),
+            googleId: Joi.string(),
+            role: Joi.string().valid("user", "data-entry", "admin"),
+            // active: Joi.boolean(),
+        }),
 };
--- server/routers/folder/folderValidation.js
+++ server/routers/folder/folderValidation.js
@@ -1,44 +1,44 @@
 const Joi = require("joi");
 
 exports.createFolderValidation = {
-  body: Joi.object().required().keys({
-    name: Joi.string().required(),
-    group: Joi.string().required(),
-    active: Joi.string().required(),
-  }),
+    body: Joi.object().required().keys({
+        name: Joi.string().required(),
+        group: Joi.string().required(),
+        // active: Joi.string().required(),
+    }),
 };
 
 exports.getOneFolderValidation = {
-  params: Joi.object().required().keys({
-    code: Joi.string().required(),
-  }),
+    params: Joi.object().required().keys({
+        code: Joi.string().required(),
+    }),
 };
 
 exports.updateOneFolderValidation = {
-  params: Joi.object().required().keys({
-    code: Joi.string().required(),
-  }),
-  body: Joi.object().required().keys({
-    size: Joi.string(),
-    name: Joi.string(),
-    group: Joi.string(),
-  }),
+    params: Joi.object().required().keys({
+        code: Joi.string().required(),
+    }),
+    body: Joi.object().required().keys({
+        size: Joi.string(),
+        name: Joi.string(),
+        group: Joi.string(),
+    }),
 };
 
 exports.deleteFolderValidation = {
-  params: Joi.object().required().keys({
-    code: Joi.string().required(),
-  }),
+    params: Joi.object().required().keys({
+        code: Joi.string().required(),
+    }),
 };
 
 exports.hideFolderValidation = {
-  body: Joi.object().required().keys({
-    active: Joi.boolean(),
-  }),
-  params: Joi.object()
-    .required()
-    .keys({
-      code: Joi.string().required(),
-    })
-    .options({ allowUnknown: true }),
+    body: Joi.object().required().keys({
+        active: Joi.boolean(),
+    }),
+    params: Joi.object()
+        .required()
+        .keys({
+            code: Joi.string().required(),
+        })
+        .options({ allowUnknown: true }),
 };
